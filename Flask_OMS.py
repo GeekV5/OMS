715e88e08133b38c035f7dcc6b56b79e4368b5af
hfp <2273885490@qq.com>
2017-05-06 22:40:10 +0800

    add

--- Flask_OMS.py
+++ Flask_OMS.py
@@ -1,7 +1,29 @@
-from flask import Flask
-from flask import redirect
-from flask import render_template
+#-*- coding:UTF-8 -*-
+from flask import Flask,request,redirect,render_template
+
+from flaskext.mysql import MySQL
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '12345678'
+app.config['MYSQL_DATABASE_DB'] = 'oms'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+mysql.init_app(app)
+
+@app.route('/mysql')
+def mysql1():
+    # （获得一个数据库操作游标）Obtain a cursor
+    # cursor = mysql.get_db().cursor()
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM oms_user")
+    #data = cursor.fetchone()
+    data = cursor.fetchall()
+    if data is None:
+        print 'OMS查询MySQL oms_user表出错！'
+    else:
+        print 'OMS查询MySQL oms_user表 结果：',data[0]
+    return render_template('test.html')
 
 # @app.route('/')
 # def hello_world():
@@ -22,6 +44,8 @@ def main():
 @app.route('/main2')
 def main2():
     return render_template('main2.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db.py
+++ db.py
@@ -6,7 +6,31 @@ from flask.ext.sqlalchemy import  SQLAlchemy
 
 # 查询数据库
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/oms'
+app.config['SECRET_KEY'] = 'hard to guess'
+
+# 这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建好的数据库名oms,连接方式参考 \
+# http://docs.sqlalchemy.org/en/latest/dialects/mysql.html
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@localhost:3306/oms'
+
+#设置这一项是每次请求结束后都会自动提交数据库中的变动
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+#实例化
 db = SQLAlchemy(app)
 
-#class User(db.Model):
+# 定义模型，建立关系
+class User(db.Model):
+    # 定义表名
+    __tablename__ = 'oms_user'
+    # 定义列对象
+    userid = db.Column(db.VARCHAR)
+    username = db.Column(db.VARCHAR)
+    password = db.Column(db.VARCHAR)
+    logintime = db.Column(db.VARCHAR)
+    rmk = db.Column(db.VARCHAR)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+if __name__=='__main__':
+    User.query.filter_by(role=U).all()  # 注意过滤器的使用
